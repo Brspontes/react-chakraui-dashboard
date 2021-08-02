20afe38db3243b7933a5aa9b7f4861aad0c92596
Brspontes <brian.robert16@hotmail.com>
2021-08-02 19:10:46 -0300

    criando sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-   <h1>DashGo</h1>
+    <Flex width="100vw" height="100vh" alignItems="center" justifyContent="center">
+      <Flex as="form" width="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column">
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail: </FormLabel>
+            <Input id="email" name="email" type="email" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" _hover={{ bgColor: "gray.900" }} size="lg" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Password: </FormLabel>
+            <Input id="password" name="password" type="password" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" _hover={{ bgColor: "gray.900" }} size="lg"/>
+          </FormControl>
+        </Stack>
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
