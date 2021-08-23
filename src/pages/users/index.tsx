f91ce3792951ab1127178dd0caab99ab6707629e
Brspontes <brian.robert16@hotmail.com>
2021-08-23 10:25:35 -0300

    prefetch de dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,23 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Td, Th, Checkbox, Tbody, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Td, Th, Checkbox, Tbody, Text, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header/";
 import Sidebar from "../../components/Sidebar";
 import Pagination from "../../components/Pagination";
-import Link from 'next/link'
+import NextLink from 'next/link'
 import { useUsers } from "../../services/hooks/useUsers";
 import { useState } from "react";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
 
+
+async function handlePrefetchUser(userId: number) {
+  await queryClient.prefetchQuery(['user', userId], async () => {
+    const response = await api.get(`users/${userId}`)
+    return response.data
+  }, {
+    staleTime: 1000 * 60 * 10
+  })
+}
 
 export default function UserList() {
   const [page, setPage] = useState(1)
@@ -28,11 +39,11 @@ export default function UserList() {
               Usuários
               {!isLoading && isFetching && <Spinner size="sm" color="gray.500" marginLeft="4" />}
             </Heading>
-            <Link href='/users/create' passHref>
+            <NextLink href='/users/create' passHref>
               <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
                 Criar Novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
           {isLoading ?(
             <Flex justify="center">
@@ -68,7 +79,9 @@ export default function UserList() {
                       </Td>
                       <Td>
                         <Box>
-                          <Text fontWeight="bold" >{user.name}</Text>
+                          <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                            <Text fontWeight="bold" >{user.name}</Text>
+                          </Link>
                           <Text fontSize="small" color="gray.300" >{user.email}</Text>
                         </Box>
                       </Td>
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -42,5 +42,5 @@ export async function getUsers(page: number): Promise<GetUserResponse> {
 }
 
 export function useUsers (page: number) {
-  return useQuery(['users', page], () => getUsers(page), { staleTime: 1000 * 5 })
+  return useQuery(['users', page], () => getUsers(page), { staleTime: 1000 * 60 * 10 })
 }
